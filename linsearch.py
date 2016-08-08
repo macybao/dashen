2ecb394a512a096b9a7b1f4fc3ff60ee5a8fa15a
Meixi Bao <mbao5@dons.usfca.edu>
2016-08-07 20:08:47 -0700

    no message

---
+++ linsearch.py
@@ -0,0 +1,30 @@
+﻿# Got slate magazine data from http://www.anc.org/data/oanc/contents/
+# rm'd .xml, .anc files, leaving just .txt
+# 4534 files in like 55 subdirs
+
+from words import get_text, words
+
+
+def linear_search(files, terms):
+    """
+    Given a list of fully-qualified filenames, return a list of them
+    whose file contents has all words in terms as normalized by your words() function.
+    Parameter terms is a list of strings.
+    Perform a linear search, looking at each file one after the other.
+    """
+    qualified = []
+
+    for file in files:
+        f = open(file)
+        try:
+            content = f.read()
+            contains = True
+            for term in terms:
+                if content.find(term) == -1:
+                    contains = False
+                    break
+            if contains:
+                qualified.append(file)
+        finally:
+            f.close()
+    return qualified
---
+++ words.py
@@ -0,0 +1,78 @@
+﻿
+import os
+import re
+import string
+
+
+def filelist(root):
+    """Return a fully-qualified list of filenames under root directory"""
+    list = []
+    if root is None:
+        return list
+    for dirpath, dirnames, filenames in os.walk(root):
+        for filename in filenames:
+            list.append(os.path.join(dirpath, filename))
+    return list
+
+
+def get_text(fileName):
+    f = open(fileName)
+    s = f.read()
+    f.close()
+    return s
+
+
+def words(text):
+    """
+    Given a string, return a list of words normalized as follows.
+    Split the string to make words first by using regex compile() function
+    and string.punctuation + '0-9\\r\\t\\n]' to replace all those
+    char with a space character.
+    Split on space to get word list.
+    Ignore words < 3 char long.
+    Lowercase all words
+    """
+    results = []
+    p = re.compile(r'[0-9\r\t\n]')
+    replaced_text = p.sub(' ', text)
+    words = replaced_text.split(' ')
+    for w in words:
+        if len(w) >= 3:
+            results.append(w)
+    return results
+
+
+
+def results(docs, terms):
+    """
+    Given a list of fully-qualifed filenames, return an HTML file
+    that displays the results and up to 2 lines from the file.
+    Return at most 100 results.  Arg terms is a list of string terms.
+    """
+    head = '<html><body><h2>Search results for <b>%s</b> in %s files</h2>' % (' '.join(terms), len(docs))
+    foot = '</body></html>'
+    content = ''
+    p = re.compile(r'\s')
+    for file in docs:
+        try:
+            content += '<p><a href="file://%s">%s</a><br>' % (file, file)
+            f = open(file)
+            count = 0
+            for line in f:
+                s = p.sub('', line)
+                if s != '' and count < 2:
+                    content += line + '<br>'
+                    count += 1
+                elif count >= 2:
+                    break
+            content += '<br>'
+        finally:
+            f.close()
+    return head + content + foot
+
+
+def filenames(docs):
+    """Return just the filenames from list of fully-qualified filenames"""
+    if docs is None:
+        return []
+    return [os.path.basename(d) for d in docs]
