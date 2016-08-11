abc9b35179e813b8b3ef5fd4990285309d0ba702
Shengwu Jiang <shengwu1@staff.weibo.com>
2016-08-12 03:25:32 +0800

    page rank

--- page_rank/Data Import.R
+++ page_rank/Data Import.R
@@ -1,6 +1,8 @@
-install.packages("multiplex")
-library(multiplex)
-data = read.gml("/Users/macy/desktop/karate/karate.gml")
-write.csv(data,"/Users/macy/desktop/network_graph.csv")
+if (!require(multiplex)) {
+    install.packages("multiplex", repos="http://cran.rstudio.com/")
+    library(multiplex)
+}
+data = read.gml( "/Users/Jiang/Documents/Develop/GitHub/dashen/page_rank/karate.gml")
+write.csv(data, "/Users/Jiang/Documents/Develop/GitHub/dashen/page_rank/network_graph.csv", row.names = FALSE)
 
 
