f722e217ac6c4dd1fa8859e5130ddc6f4c174cd4
Meixi Bao <mbao5@dons.usfca.edu>
2016-08-23 16:41:33 -0700

    Moved final version code

--- sum.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        x = float(raw_input("Please enter the first number: "))
-        y = float(raw_input("Please enter the second number: "))
-        print "The sum of", x, "and", y, "is:", x+y
-        break
-    except ValueError:
-        print "Please enter a NUMBER!"
-        continue
